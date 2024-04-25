b0a4bd0c28eb78711a4a289d3f6e61ee47be2106
kevinopix <kevinopix@gmail.com>
2024-04-24 22:21:10 -0500

    Symbols Data Import Commands

---
+++ company/management/commands/import_companies.py
@@ -0,0 +1,22 @@
+import csv
+from django.core.management.base import BaseCommand
+from company.models import Company
+
+class Command(BaseCommand):
+    help = 'Import data from a CSV file'
+
+    def add_arguments(self, parser):
+        parser.add_argument('csv_file', type=str, help='Path to the CSV file')
+
+    def handle(self, *args, **kwargs):
+        csv_file = kwargs['csv_file']
+
+        with open(csv_file, 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                my_model_instance = Company(
+                    symbol=row['Symbol'],
+                )
+                my_model_instance.save()
+
+        self.stdout.write(self.style.SUCCESS('Data imported successfully'))
