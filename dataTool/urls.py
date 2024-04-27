408dad01bff34b2199f945efcec92bda83234971
kevinopix <kevinopix@gmail.com>
2024-04-26 19:40:11 -0500

    dataTool app

---
+++ dataTool/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+# from django.views.generic.base import View
+# from .views import CompanyAllView
+
+urlpatterns = [
+    # path('', CompanyAllView.as_view(), name='home'),
+]
--- yahoofinanceapp/settings.py
+++ yahoofinanceapp/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     "company.apps.CompanyConfig",
+    "dataTool.apps.DatatoolConfig",
 ]
 
 MIDDLEWARE = [
