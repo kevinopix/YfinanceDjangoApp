efad1534ea659f83ea132a85fffde19bc80e53e0
kevinopix <kevinopix@gmail.com>
2024-04-26 19:52:53 -0500

    dataTool app

--- dataTool/views.py
+++ dataTool/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import View
+from company.models import Company
 
 # Create your views here.
+class checkNewRecords(View):
+    template_name = 'dataTool/check_new_records.html'
+
+    def get(self,request,*args,**kwargs):
+        companies = Company.objects.all()
+        context={
+            'all_companies': companies 
+        }
+        print(context)
+        return render(request, self.template_name,context)
