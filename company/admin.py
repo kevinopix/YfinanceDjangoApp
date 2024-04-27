a801252d3b1d9e94c8413a7179889592cbc64305
kevinopix <kevinopix@gmail.com>
2024-04-26 20:00:19 -0500

    dataTool app

--- company/admin.py
+++ company/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import Company, StockInfo
 
 # Register your models here.
-admin.site.register(Company)
-admin.site.register(StockInfo)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ("symbol_val", "title")
+
+class StockInfoAdmin(admin.ModelAdmin):
+    list_display = ("symbol", "date", "Open", "Close", "Volume")
+
+
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(StockInfo, StockInfoAdmin)
